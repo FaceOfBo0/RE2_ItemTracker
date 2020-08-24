f019e3c0bf460ca195fb03593eb1214dd0927987
FaceOfBo0 <70140104+FaceOfBo0@users.noreply.github.com>
2020-08-24 15:40:01 +0200

    Add files via upload

---
+++ RE2_ItemTracker/Loader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+
+namespace RE2_ItemTracker
+{
+    class Loader
+    {
+        public Loader() { }
+
+        public static void LoadNewWindow (Window a_old, Window a_new, DependencyProperty Height, DependencyProperty Width)
+        {
+            // Get absolute location on screen of upper left corner of window
+            Point locationFromScreen = a_old.PointToScreen(new Point(0, 0));
+
+            a_new.Top = locationFromScreen.Y;
+            a_new.Left = locationFromScreen.X;
+            a_new.SetValue(Height, a_old.Height);
+            a_new.SetValue(Width, a_old.Width);
+            a_new.Show();
+            a_old.Close();
+        }
+    }
+
+
+}
